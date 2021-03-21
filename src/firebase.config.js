d186c2cee6b27b7394c3d44c8d235b40a6f8bbc5
anik055 <59714409+anik055@users.noreply.github.com>
2021-03-21 08:16:12 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAGxKOr8Ji2YHp3sKf7y3VljdQC6QVSL5s",
+    authDomain: "bullet-vai.firebaseapp.com",
+    projectId: "bullet-vai",
+    storageBucket: "bullet-vai.appspot.com",
+    messagingSenderId: "1024478302865",
+    appId: "1:1024478302865:web:c1f23fc7141c1cf812a530"
+  };
+
+  export default firebaseConfig;
